60b04c9a7ee361d408ae101232e9ab5ac845484a
Johnny Kiely <johnnykiely@gmail.com>
2016-08-18 17:01:13 +0100

    From humble beginings

---
+++ Assets/01. Scripts/Grids/CellInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellInput : MonoBehaviour
+{
+
+	void OnMouseDown ()
+	{
+		Debug.Log ("Transform position: " + this.transform.position.ToString ());
+	}
+
+}
---
+++ Assets/01. Scripts/Grids/SectorCell.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gamelogic.Grids;
+using Gamelogic;
+
+
+public class SectorCell : SpriteCell
+{
+
+	public bool isAccessible = true;
+	public float Cost = 1;
+	public Contents contents;
+
+
+
+
+
+
+
+	public enum Contents
+	{
+		empty,
+		unit
+
+	}
+}
+
